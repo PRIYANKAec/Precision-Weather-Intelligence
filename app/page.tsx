335e570d87d90030421dbf547a0a450bc26924ac
PRIYANKAec <priyankaa261103@gmail.com>
2024-06-17 14:13:43 +0530

    Sunrise And Sunset Time[ADD]

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import AirPollution from "./Components/AirPollution/AirPollution";
 import Navbar from "./Components/Navbar";
+import Sunset from "./Components/Sunset/Sunset";
 import Temperature from "./Components/Temperature/Temperature";
+
 export default function Home() {
   return (
     <main className="mx-[1rem] lg:mx-[2rem] xl:mx-[6rem] 2xl:mx-[16rem] m-auto">
@@ -12,6 +14,7 @@ export default function Home() {
         <div className="flex flex-col "></div>
         <div className="instruments grid h-full gap-4 col-span-full sm-2:col-span-2 lg:grid-cols-3 xl:grid-cols-4">
         <AirPollution />
+        <Sunset />
         </div>
       </div>
     </main>
