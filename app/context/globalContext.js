d449a5c6ff2d321f29e27765b894e1ff165abba3
PRIYANKAec <priyankaa261103@gmail.com>
2024-06-18 07:41:18 +0530

    Implemented five day forecast through API fetch[ADD]

--- app/context/globalContext.js
+++ app/context/globalContext.js
@@ -10,11 +10,12 @@ export const GlobalContextProvider = ({ children }) => {
   const [forecast, setForecast] = useState({});
 
   const [airQuality, setAirQuality] = useState({});
+  const [fiveDayForecast, setFiveDayForecast] = useState({});
 
   const fetchForecast = async (lat, lon) => {
     try {
       const res = await axios.get(`api/weather?lat=${lat}&lon=${lon}`);
-            
+        console.log("forecast data", res.data);
       setForecast(res.data);
     } catch (error) {
       console.log("Error fetching forecast data: ", error.message);
@@ -24,16 +25,30 @@ export const GlobalContextProvider = ({ children }) => {
   const fetchAirQuality= async (lat, lon) => {
     try {
       const res = await axios.get(`api/pollution`);
-            
+
+    console.log("air quality data: ", res.data);
       setAirQuality(res.data);
     } catch (error) {
       console.log("Error fetching air quality data: ", error.message);
     }
   };
 
+  const fetchFiveDayForecast = async (lat, lon) => {
+    try {
+      const res = await axios.get(`api/fiveday`);
+
+      console.log("five day forecast data: ", res.data);
+
+      setFiveDayForecast(res.data);
+    } catch (error) {
+      console.log("Error fetching five day forecast data: ", error.message);
+    }
+  };
+
   useEffect(() => {
     fetchForecast("11.504776","77.238396");
     fetchAirQuality();
+    fetchFiveDayForecast();
      }, []);
     
   return (
@@ -41,13 +56,14 @@ export const GlobalContextProvider = ({ children }) => {
       value={{
         forecast,
         airQuality,
+        fiveDayForecast,
       }}
     >
       <GlobalContextUpdate.Provider>
         {children}
       </GlobalContextUpdate.Provider>
     </GlobalContext.Provider>
-  );
+);
 };
 
 export const useGlobalContext = () => useContext(GlobalContext);
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import AirPollution from "./Components/AirPollution/AirPollution";
+import DailyForecast from "./Components/DailyForecast/DailyForecast";
 import Navbar from "./Components/Navbar";
 import Sunset from "./Components/Sunset/Sunset";
 import Temperature from "./Components/Temperature/Temperature";
@@ -17,6 +18,7 @@ export default function Home() {
         <AirPollution />
         <Sunset />
         <Wind />
+        <DailyForecast />
         </div>
       </div>
     </main>
