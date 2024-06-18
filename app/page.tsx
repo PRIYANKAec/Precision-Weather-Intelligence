40bc7932bd6b15de0eebe732f826c3f9117178a6
PRIYANKAec <priyankaa261103@gmail.com>
2024-06-18 15:28:00 +0530

    Implemented Map Visualise by API Fetch[ADD]

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import AirPollution from "./Components/AirPollution/AirPollution";
 import DailyForecast from "./Components/DailyForecast/DailyForecast";
 import FeelsLike from "./Components/FeelsLike/FeelsLike";
 import Humdity from "./Components/Humdity/Humdity";
+import Mapbox from "./Components/Mapbox/Mapbox";
 import Navbar from "./Components/Navbar";
 import Population from "./Components/Population/Population";
 import Pressure from "./Components/Pressure/Pressure";
@@ -10,16 +11,17 @@ import Temperature from "./Components/Temperature/Temperature";
 import UvIndex from "./Components/UvIndex/UvIndex";
 import Visibility from "./Components/Visibility/Visibility";
 import Wind from "./Components/Wind/Wind";
+import defaultStates from "./utils/defaultStates";
 
 export default function Home() {
   return (
     <main className="mx-[1rem] lg:mx-[2rem] xl:mx-[6rem] 2xl:mx-[16rem] m-auto">
       <Navbar />
       <div className="pb-4 flex flex-col gap-4 md:flex-row">
-        <div className="flext flex-col gap-4 w-full min-w-[18rem] md:w-[35rem]">
-          <Temperature />
-        </div>
-        <div className="flex flex-col "></div>
+            <div className="flext flex-col gap-4 w-full min-w-[18rem] md:w-[35rem]">
+              <Temperature />
+            </div>
+        <div className="flex flex-col ">
         <div className="instruments grid h-full gap-4 col-span-full sm-2:col-span-2 lg:grid-cols-3 xl:grid-cols-4">
         <AirPollution />
         <Sunset />
@@ -32,7 +34,28 @@ export default function Home() {
         <Visibility />
         <Pressure />
         </div>
-      </div>
+         <div className="mapbox-con mt-4 flex gap-4">
+            <Mapbox />
+            <div className="states flex flex-col gap-3 flex-1">
+              <h2 className="flex items-center gap-2 font-medium">
+                Top Large Cities
+              </h2>
+              <div className="flex flex-col gap-4">
+                {defaultStates.map((state, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className="border rounded-lg cursor-pointer dark:bg-dark-grey shadow-sm dark:shadow-none"
+                    >
+                      <p className="px-6 py-4">{state.name}</p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+          </div>
+          </div>
     </main>
   );
 }
