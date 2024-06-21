70e93f568fac99cc6df48564dddf3dde73f08e35
PRIYANKAec <priyankaa261103@gmail.com>
2024-06-21 14:27:42 +0530

    CommandInput Server-Client error @MapBox.js[FIX]

--- app/Components/Mapbox/Mapbox.js
+++ app/Components/Mapbox/Mapbox.js
@@ -1,13 +1,18 @@
 "use client";
 
-import React, { useEffect } from "react";
-import { MapContainer, TileLayer, useMap } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+import React, { useEffect, useState } from "react";
+import dynamic from 'next/dynamic';
 import { useGlobalContext } from "@/app/context/globalContext";
+import { useMapEvents } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
-// @ts-ignore
-function FlyToActiveCity({ activeCityCords }) {
-  const map = useMap();
+const MapContainer = dynamic(() => import('react-leaflet').then(mod => mod.MapContainer), { ssr: false });
+const TileLayer = dynamic(() => import('react-leaflet').then(mod => mod.TileLayer), { ssr: false });
+
+const FlyToActiveCity = ({ activeCityCords }) => {
+  const map = useMapEvents({
+    load: () => console.log('Map loaded')
+  });
 
   useEffect(() => {
     if (activeCityCords) {
@@ -25,10 +30,15 @@ function FlyToActiveCity({ activeCityCords }) {
   }, [activeCityCords, map]);
 
   return null;
-}
+};
 
-function Mapbox() {
-  const { forecast } = useGlobalContext(); // Your coordinates
+const Mapbox = () => {
+  const { forecast } = useGlobalContext();
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   const activeCityCords = forecast?.coord;
 
@@ -42,22 +52,24 @@ function Mapbox() {
 
   return (
     <div className="flex-1 basis-[50%] border rounded-lg">
-      <MapContainer
-        center={[activeCityCords.lat, activeCityCords.lon]}
-        zoom={13}
-        scrollWheelZoom={false}
-        className="rounded-lg m-4"
-        style={{ height: "calc(100% - 2rem)", width: "calc(100% - 2rem)" }}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
+      {isClient && (
+        <MapContainer
+          center={[activeCityCords.lat, activeCityCords.lon]}
+          zoom={13}
+          scrollWheelZoom={false}
+          className="rounded-lg m-4"
+          style={{ height: "calc(100% - 2rem)", width: "calc(100% - 2rem)" }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
 
-        <FlyToActiveCity activeCityCords={activeCityCords} />
-      </MapContainer>
+          <FlyToActiveCity activeCityCords={activeCityCords} />
+        </MapContainer>
+      )}
     </div>
   );
-}
+};
 
 export default Mapbox;
