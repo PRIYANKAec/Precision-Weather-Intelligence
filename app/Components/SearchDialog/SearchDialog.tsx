4a2ce7c944badd997994f1adc511374f51a5609b
PRIYANKAec <priyankaa261103@gmail.com>
2024-06-21 15:10:18 +0530

    CommandInput Server-Client error @SearchDialog.js[FIX]

--- app/Components/SearchDialog/SearchDialog.tsx
+++ app/Components/SearchDialog/SearchDialog.tsx
@@ -5,7 +5,11 @@ import {
 } from "@/app/context/globalContext";
 import { commandIcon } from "@/app/utils/Icons";
 import { Button } from "@/components/ui/button";
-import { Command, CommandInput } from "@/components/ui/command";
+import {
+  Command,
+  CommandInput,
+  CommandList,
+} from "@/components/ui/command";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import React from "react";
 
@@ -18,14 +22,14 @@ function SearchDialog() {
   const getClickedCoords = (lat: number, lon: number) => {
     setActiveCityCoords([lat, lon]);
   };
-
   return (
     <div className="search-btn">
       <Dialog>
         <DialogTrigger asChild>
           <Button
             variant="outline"
-            className="border inline-flex items-center justify-center text-sm font-medium hover:dark:bg-[#131313] hover:bg-slate-100  ease-in-out duration-200">
+            className="border inline-flex items-center justify-center text-sm font-medium hover:dark:bg-[#131313] hover:bg-slate-100  ease-in-out duration-200"
+          >
             <p className="text-sm text-muted-foreground">Search Here...</p>
             <div className="command dark:bg-[#262626] bg-slate-200  py-[2px] pl-[5px] pr-[7px] rounded-sm ml-[10rem] flex items-center gap-2">
               {commandIcon}
@@ -35,7 +39,7 @@ function SearchDialog() {
         </DialogTrigger>
 
         <DialogContent className="p-0">
-          <Command className=" rounded-lg border shadow-md">
+          <Command className="rounded-lg border shadow-md">
             <CommandInput
               value={inputValue}
               onChangeCapture={handleInput}
@@ -79,6 +83,7 @@ function SearchDialog() {
                   }
                 )}
             </ul>
+            <CommandList></CommandList>
           </Command>
         </DialogContent>
       </Dialog>
